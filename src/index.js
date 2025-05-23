93bc99f5c5776a3da1749e251b6f9329a22c9cfa
Charlie Lilly <c.schulz99@gmx.net>
2025-05-23 12:57:07 +0200

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 function refreshWeather(response) {
     let temperatureElement = document.querySelector("#weather-app-temperature");
     let cityElement = document.querySelector("#weather-app-city");
+    let descriptionElement = document.querySelector("#weather-condition-description");
+    let humidtiyElement = document.querySelector("#weather-humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+
     cityElement.innerHTML = response.data.city;
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidtiyElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    timeElement.innerHTML = formatDate(date);
     temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+}
+
+function formatDate(date) {
+    let hour = date.getHours();
+    let minutes = date.getMinutes();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hour}:${minutes}`
 }
 
 function searchCity(city) {
