e0691207fc9ef8b4c8cf11a7329838e565091ab3
Charlie Lilly <c.schulz99@gmx.net>
2025-05-23 13:21:04 +0200

    coded a changeable weather icon

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ function refreshWeather(response) {
     let windSpeedElement = document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
+    let iconElement = document.querySelector("#icon");
 
     cityElement.innerHTML = response.data.city;
     descriptionElement.innerHTML = response.data.condition.description;
@@ -13,6 +14,7 @@ function refreshWeather(response) {
     windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
     timeElement.innerHTML = formatDate(date);
     temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
 
 function formatDate(date) {
